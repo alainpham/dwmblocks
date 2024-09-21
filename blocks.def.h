a19d6b87a644cc613fb139dbe1019d5290bdf6e5
Alain <alainphm@gmail.com>
2024-09-21 17:35:40 +0200

    fix

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
         /*Icon*/        /*Command*/                     /*Update Interval*/ 	/*Update Signal*/
-        {"",        	"bar mem",             			5,						0},
-		{"",        	"bar temp", 					5,  					0},
-        {"",        	"bar bat", 						60, 					0},
-        {"",            "bar net",						5,  					0},
-        {"",            "date '+%Y-%d-%m %T %Z'",       1,  					0},
+        {"",        	"bar memory",          			5,						0},
+		{"",        	"bar cputemp", 					5,  					0},
+        {"",        	"bar battery",					60, 					0},
+        {"",            "bar network",					5,  					0},
+        {"",            "bar date",       1,  					0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
